8cb77093730c6b943c9569461716800874ddb49b
pc1_3 <pc1_3@DESKTOP-4HRIQMN>
2023-11-28 16:05:16 +0200

    Add project files.

---
+++ formsapps/StalinWay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formsapps
+{
+    public partial class StalinWay : Form
+    {
+        public StalinWay()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stalinarsenal Stalin = new Stalinarsenal();
+            Stalin.Show();
+            Hide();
+        }
+    }
+}
